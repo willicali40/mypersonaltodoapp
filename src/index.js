1a52c58380e4d8ad3a353bd357c6f88324f81435
William david <Willicali40@hotmail.com>
2023-04-28 17:59:57 -0500

    data is store in localStorage when task is created, task is mark as completed and when task is deleted

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const inputTask = document.querySelector("#inputTask");
 const ul = document.querySelector("#ul");
+const btn = document.querySelector('button')
 
 let task = "";
 
@@ -11,36 +12,73 @@ inputTask.addEventListener("input", (e) => {
 
 inputTask.addEventListener("keydown", (e) => {
   if (e.key == "Enter") {
+    pushData();
     createTask(task);
   }
 });
 
+let identifier = 0;
+
+let data = []
+
+function pushData() {
+    data.push({
+        id: identifier,
+        status: 'pending',
+        task: task,
+    })
+
+    storageLocalData()
+}
+
+function storageLocalData() {
+    localStorage.setItem('data', JSON.stringify(data))
+}
+
 function createTask(task) {
   const template = `
-    <li status="pending">
+    <li status="pending" id="${identifier}">
         <input type="checkbox" state="false" id="checkbox">
         <label state="false" id="label">${task}</label>
-        <i class="fa-solid fa-xmark" state="false" id="close"></i>
+        <i class="fa-solid fa-xmark" state="false" id="delete"></i>
     </li>
     `;
     ul.insertAdjacentHTML('beforeend', template)
     inputTask.value = '';
+    identifier++
+    console.log(data);
 }
 
 ul.addEventListener('click', (e) => {
-    const element = e.target
-    const parentElement = element.parentNode
+    const element = e.target;
+    const parentElement = element.parentNode;
+    const parentElementId = parentElement.attributes.id.value
+    const label = parentElement.querySelector('#label');
 
     if (element.attributes.id.value === 'checkbox') {
         if (parentElement.attributes.status.value === 'pending') {
             parentElement.attributes.status.value = 'completed'
-            const label = parentElement.querySelector('#label');
+            data[parentElementId].status = 'completed'
+            console.log(data);
             label.style.textDecoration = 'line-through'
+            storageLocalData()
         } else if (parentElement.attributes.status.value === 'completed') {
-            parentElement.attributes.status.value = 'pending'
-            label.style.textDecoration = 'none'
+            parentElement.attributes.status.value = 'pending';
+            label.style.textDecoration = 'none';
+            data[parentElementId].status = 'pending'
+            console.log(data);
+            storageLocalData()
         }
-    } else if (e.target.attributes.id.value === 'close') {
+    } else if (e.target.attributes.id.value === 'delete') {
         parentElement.remove()
+        data.splice(parentElementId, 1)
+        storageLocalData()
+        console.log(data);
     }
+})
+
+// localStorage.clear()
+
+btn.addEventListener('click', () => {
+    console.log(JSON.parse(localStorage.getItem('data')));
 })
